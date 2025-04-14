bb1929850c5a3d3e3c1c2390d46086df3d0ea30d
matteodragotto <matteodrag8@gmail.com>
2025-04-15 00:36:15 +0200

    [feat] complete layouts for header and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,8 +33,58 @@
     </div>
 </div>
 
-<div class="middle-nav">
-    
+<div class="middle-nav bg-primary py-5 mt-5 overflow-auto">
+  <div class="container d-flex justify-content-between">
+
+    <div class="blue-nav d-flex justify-content-center align-items-center gap-2">
+
+        <div class="nav-img">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="buy-comics-digital-comics">
+        </div>
+
+        <p class="fw-medium text-light m-0">DIGITAL COMICS</p>
+    </div>
+
+    <div class="blue-nav d-flex justify-content-center align-items-center gap-2">
+
+        <div class="nav-img">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="buy-comics-merchandise">
+
+        </div>
+
+        <p class="fw-medium text-light m-0">DC MERCHANDISE</p>
+    </div>
+
+    <div class="blue-nav d-flex justify-content-center align-items-center gap-2">
+
+        <div class="nav-img">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="buy-comics-subscriptions">
+        </div>
+
+        
+        <p class="fw-medium text-light m-0">SUBSCRIPTION</p>
+    </div>
+
+    <div class="blue-nav d-flex justify-content-center align-items-center gap-2">
+
+        <div class="nav-img">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="buy-comics-shop-locator">
+
+        </div>
+
+        <p class="fw-medium text-light m-0">COMIC SHOP LOCATOR</p>
+    </div>
+
+    <div class="blue-nav d-flex justify-content-center align-items-center gap-2">
+
+        <div class="nav-img">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="buy-dc-power-visa">
+        </div>
+        <p class="fw-medium text-light m-0">DC POWER VISA</p>
+        
+    </div>
+
+  </div>
 </div>
 
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,91 @@
 <footer>
-  <h1>Questo è il footer</h1>
+  <div class="footer-nav">
+    <div class="container d-flex justify-content-between py-4 position-relative overflow-hidden">
+      <div class="row row-cols-3">
+
+        <div class="col">
+            <ul class="text-secondary list-unstyled">
+            <h5 class="text-light">DC COMICS</h5>
+            <li><a href="#" class="text-secondary text-decoration-none">Characters</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">Comics</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">Movies</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">TV</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">Games</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">Videos</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">News</a></li>
+            </ul>
+            <ul class="text-secondary list-unstyled">
+            <h5 class="text-light">SHOP</h5>
+            <li><a href="#" class="text-secondary text-decoration-none">Shop DC</a></li>
+            <li><a href="#" class="text-secondary text-decoration-none">Shop DC Collectibles</a></li>           
+            </ul>
+        </div>
+
+        <div class="col">
+          <ul class="text-secondary list-unstyled">
+          <h5 class="text-light">DC</h5>
+          <li><a href="#" class="text-secondary text-decoration-none">Terms Of Use</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Privacy policy</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Ad Choices</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Advertising</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Jobs</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Subscriptions</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Talent Workshops</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">CPSC Certificates</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Ratings</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Shop Help</a></li>
+          <li><a href="#" class="text-secondary text-decoration-none">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div class="col">
+        <ul class="text-secondary list-unstyled">
+        <h5 class="text-light">SITES</h5>
+        <li><a href="#" class="text-secondary text-decoration-none">DC</a></li>
+        <li><a href="#" class="text-secondary text-decoration-none">MAD Magazine</a></li>
+        <li><a href="#" class="text-secondary text-decoration-none">DC Kids</a></li>
+        <li><a href="#" class="text-secondary text-decoration-none">DC Universe</a></li>
+        <li><a href="#" class="text-secondary text-decoration-none">DC Power Visa</a></li>
+        </ul>
+        </div>
+      </div>
+      <div class="footer-logo position-absolute top-50 end-0 translate-middle-y z-0">
+        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="logo-bg-dc">
+      </div>
+  
+    </div>
+   
+  </div>
+  <div class="footer-socials py-4">
+    <div class="container d-flex justify-content-between">
+      <div class="sign-up">
+        <a href="#" class="text-light fw-bold fs-5 text-decoration-none border border-2 border-primary px-3 py-2">SIGN-UP NOW!</a>      
+      </div>
+      <div class="socials d-flex justify-content-around align-items-center gap-2">
+        <h3 class="text-primary m-0">FOLLOW US</h3>
+
+        <a href="#">    
+          <img src="{{ Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook">
+        </a>
+
+        <a href="#">    
+          <img src="{{ Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter">
+        </a>
+
+        <a href="#">    
+          <img src="{{ Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube">
+        </a>
+
+        <a href="#">    
+          <img src="{{ Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest">
+        </a>
+
+        <a href="#">    
+          <img src="{{ Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope">
+        </a>
+
+      </div>
+    </div>
+    
+  </div>
 </footer>
